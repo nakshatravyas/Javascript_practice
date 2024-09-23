901a37401b904ee25c85679424f26c816662a07f
Nakshatra Vyas <nakshatravyas2004@gmail.com>
2024-09-23 18:18:24 +0530

    done with datatypes

---
+++ 1_Basics/2_dataTypes.js
@@ -0,0 +1,17 @@
+// "use strict"; treat all js code as newer version
+
+let name="nakshatra"
+let age=19
+let isLoggedIn=false
+
+//number =>2^53
+//bigint
+//string =>" "
+//boolean =>true/false
+//null => it is a datatype and standalone value as well/representation of empty value but the value exist
+//undefined => value is not assigned
+//symbol => used uniqueness
+//object => 
+    console.log(typeof age) //number
+    console.log(typeof null) //obj
+    console.log(typeof undefined) //undefined
