51d625e90d651a8d43474213cf3a3ca79cbb333e
Nakshatra Vyas <nakshatravyas2004@gmail.com>
2024-09-23 18:46:44 +0530

    done with conversion of datatypes

---
+++ 1_Basics/3_conversionOperation.js
@@ -0,0 +1,70 @@
+let score="33a"
+
+console.log(typeof score)
+
+//string to number
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//here the 33a which is not a pure number but then also it will be converted in number in js but seeing the value of converted is NaN (may be a issue, solution is typescript)
+//string cant be converted in number gives Nan as value
+
+console.log("---------------------------Null---------------------------------")
+
+let score1=null
+console.log(typeof score1)
+//string to number
+let valueInNull=Number(score1)
+console.log(typeof valueInNull)
+console.log(valueInNull)
+
+console.log("---------------------------Undefined---------------------------------")
+
+let score2=undefined
+console.log(typeof score2)
+//string to number
+let valueInun=Number(score2)
+console.log(typeof valueInun)
+console.log(valueInun)
+
+console.log("---------------------------Boolean---------------------------------")
+
+let score3=true
+console.log(typeof score3)
+//string to number
+let valueInbol=Number(score3)
+console.log(typeof valueInbol)
+console.log(valueInbol)
+
+//"33" -> 33
+//"33abc" -> NaN
+//true -> 1; false -> 0
+//null -> 0
+
+console.log("---------------------------Number to Boolean---------------------------------")
+let isLoggedIn=1
+let boolValue=Boolean(isLoggedIn)
+console.log(boolValue)
+
+let isLoggedIn1=""
+let boolValue1=Boolean(isLoggedIn1)
+console.log(boolValue1)
+
+let isLoggedIn2="hello"
+let boolValue2=Boolean(isLoggedIn2)
+console.log(boolValue2)
+
+//1->true
+//0->false
+//""->false
+//"hjdhjs"->true
+
+console.log("---------------------------Number to String---------------------------------")
+let num=33
+console.log(typeof num)
+//string to number
+let numToString=String(num)
+console.log(typeof numToString)
+console.log(numToString)
+
